7e3ae9eb080c495bcb0a4d09e990b65cf08dd3d1
hamoncode <112704332+hamoncode@users.noreply.github.com>
2024-05-10 17:11:33 -0400

    Update install_script1.sh

--- install_script1.sh
+++ install_script1.sh
@@ -7,6 +7,16 @@ sudo apt update && sudo apt upgrade
 # Install essential packages 
 sudo apt install -y git gh wget zsh 
 
+# git config 
+
+git config --global user.name "hamoncode"
+git config --global user.email "vincent.hamon23@gmail.com"
+git config --global core.editor "nvim"
+
+echo "please provide input for configuration of git hub CLI"
+
+gh auth login 
+
 echo "please provide input for configuration of ohmyzsh"
 
 # Install Oh My Zsh
