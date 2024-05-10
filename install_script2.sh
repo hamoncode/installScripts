ee266ff9ce2e48d5cb02bf5eb4d9a2fc31b8053a
hamoncode <112704332+hamoncode@users.noreply.github.com>
2024-05-10 17:29:00 -0400

    Update install_script2.sh

--- install_script2.sh
+++ install_script2.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
-# Install nvim with astroNvim package
-sudo apt install neovim
-git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
-rm -rf ~/.config/nvim/.git
 
 # Install additional apt packages
-sudo apt install -y build-essential curl flatpak gh kitty librewolf neofetch nodejs npm pandoc pipewire ripgrep snapd tmux wireplumber xclip zathura zathura-pdf-poppler
+sudo apt install -y build-essential curl flatpak gh kitty librewolf neofetch nodejs npm pandoc pipewire ripgrep tmux wireplumber xclip zathura zathura-pdf-poppler
 
 # Install snap packages
+sudo apt install snapd
+sudo snap install core
 sudo snap install discord intellij-idea-community nordpass spotify zoom-client
 
+# Install nvim with astroNvim package
+sudo snap install nvim --classic
+rm -rf ~/.config/nvim
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+rm -rf ~/.config/nvim/.git
