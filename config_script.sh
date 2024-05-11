ebe6f43eed032689bca5fe7fb2796a8556a3aae3
hamoncode <112704332+hamoncode@users.noreply.github.com>
2024-05-10 23:31:45 -0400

    Update config_script.sh

--- config_script.sh
+++ config_script.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
 # permalink pour .zshrc file
-rm -rf ~/.zshrc || echo { "erreur pas de zshrc trouvé"; exit 1; }
+rm -rf ~/.zshrc ||  { echo "erreur pas de zshrc trouvé"; exit 1; }
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 # permalink pour les desktops shortcut
 
-rm -rf ~/.local/share/applications || echo { "erreur pas de folder applications trouvé"; exit 1; }
+rm -rf ~/.local/share/applications ||  { echo "erreur pas de folder applications trouvé"; exit 1; }
 ln -s ~/.dotfiles/applications ~/.local/share/applications
 
 # permalink pour les configurations du terminale
-rm -rf ~/.config/kitty || echo { "erreur pas de folder applications trouvé"; exit 1; }
+rm -rf ~/.config/kitty ||  { echo "erreur pas de folder applications trouvé"; exit 1; }
 ln -s ~/.dotfiles/kitty ~/.config/kitty
