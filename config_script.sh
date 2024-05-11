5a6e39b89217b4d0797dd23358832de040b4f564
hamoncode <112704332+hamoncode@users.noreply.github.com>
2024-05-10 21:53:25 -0400

    Update config_script.sh

--- config_script.sh
+++ config_script.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
-
-
 # permalink pour .zshrc file
-rm -rf ~/.zshrc || echo <"erreur pas de zshrc trouvé" exit 1>
+rm -rf ~/.zshrc || echo {"erreur pas de zshrc trouvé"; exit 1;}
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 # permalink pour les desktops shortcut
 
-rm -rf ~/.local/share/applications
+rm -rf ~/.local/share/applications || echo {"erreur pas de folder applications trouvé"; exit 1;}
 ln -s ~/.dotfiles/application ~/.local/share/applications
