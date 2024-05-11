0d6048b4ca38b6b10939f32c584a7370ff438e1e
hamoncode <112704332+hamoncode@users.noreply.github.com>
2024-05-10 23:24:40 -0400

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -38,14 +38,14 @@ rm -rf ~/.config/nvim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim || { echo "Error cloning AstroNvim template"; exit 1; }
 rm -rf ~/.config/nvim/.git
 
-# install omz external package
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/marlonrichert/zsh-autocomplete.git 
-
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
 echo "Please provide input for configuration of Oh My Zsh"
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" || { echo "Error installing Oh My Zsh"; exit 1; }
+
+# install omz external package
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/marlonrichert/zsh-autocomplete.git 
 
 echo "Installation completed successfully!"
