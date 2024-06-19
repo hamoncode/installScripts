8f199e9745e6d55ecb344748a98ebaa0359c01ce
hamoncode <vincent.hamon23@gmail.com>
2024-06-18 21:42:09 -0400

    for loop parser build done

--- install_script.sh
+++ install_script.sh
@@ -1,42 +1,54 @@
 #!/bin/bash
 
+# files for parsing
+PACKAGE_APT="packageApt.txt"
+PACKAGE_SNAP="packageSnap.txt"
+
+# vérification that files exists
+if [[ ! -f "$PACKAGE_APT"]];
+  echo "error: $PACKAGE_APT not found"
+  exit 1
+fi
+if [[ ! -f "$PACKAGE_SNAP"]];
+  echo "error: $PACKAGE_SNAP not found"
+  exit 1
+fi
+
+# parser avec cat les fichier
+PackagesApt=$(cat "PACKAGE_APT")
+PackagesSnap=$(cat "PACKAGE_SNAP")
+
 # Update and upgrade apt package lists
 echo "Updating and upgrading apt package lists..."
 sudo apt update && sudo apt upgrade || { echo "Error updating and upgrading apt packages"; exit 1; }
 
-# Install essential packages
-echo "Installing essential packages..."
-sudo apt install -y git gh wget || { echo "Error installing essential packages"; exit 1; }
+# Install apt packages
+echo "Installing apt packages..."
+for package in $PackagesApt; do
+  sudo apt install -y "$package" || { echo "Error installing $package"; exit 1; }
+done
+
+# Install snap packages
+echo "Installing snap packages..."
+for package in $PackagesSnap; do
+  sudo apt install -y "$package" || { echo "Error installing $package"; exit 1; }
+done
+
+# Update package lists
+echo "Updating package lists..."
+sudo apt update
+sudo snap update
 
 # Configure GitHub CLI
 echo "Configuring GitHub CLI..."
 echo "Please provide input for configuration of GitHub CLI"
 gh auth login || { echo "Error configuring GitHub CLI"; exit 1; }
 
-# Update package lists
-echo "Updating package lists..."
-sudo apt update
-
-# Install additional apt packages
-echo "Installing additional apt packages..."
-sudo apt install -y build-essential curl chromium flatpak kitty neofetch nodejs npm pandoc pipewire ripgrep tmux wireplumber xclip zathura zathura-pdf-poppler || { echo "Error installing apt packages"; exit 1; }
-
-# Install snap packages
-echo "Installing snap packages..."
-sudo apt install snapd || { echo "Error installing snapd"; exit 1; }
-sudo snap install core || { echo "Error installing snap core"; exit 1; }
-sudo snap install discord || { echo "Error installing discord"; exit 1; }
-sudo snap install nordpass || { echo "Error installing nordpass"; exit 1; }
-sudo snap install spotify || { echo "Error installing spotify"; exit 1; }
-sudo snap install zoom-client || { echo "Error installing zoom"; exit 1; }
-sudo snap install intellij-idea-community --classic || { echo "error installing intellij"; exit 1; }
-
-# Install nvim with astroNvim package
+# configure nvim with astroNvim package
 echo "Installing nvim with AstroNvim package..."
-sudo snap install nvim --classic || { echo "Error installing nvim"; exit 1; }
-rm -rf ~/.config/nvim
+rm -rf ~/.config/nvim || { echo "error removing nvim config"; exit 1; }
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim || { echo "Error cloning AstroNvim template"; exit 1; }
-rm -rf ~/.config/nvim/.git
+rm -rf ~/.config/nvim/.git 
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
@@ -45,3 +57,4 @@ echo "Please provide input for configuration of Oh My Zsh"
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" || { echo "Error installing Oh My Zsh"; exit 1; }
 
 echo "Installation completed successfully!"
+echo "now would be a good idea to reboot"
