a3a5cdcb07fa33555060a77235f398a12f72a5a8
hamoncode <vincent.hamon23@gmail.com>
2025-02-10 14:14:44 -0500

    fix probleme zsh sortie de script

--- install_script_ubuntu.sh
+++ install_script_ubuntu.sh
@@ -63,4 +63,45 @@ cp build/src /usr/local/bin
 echo "Installing Oh My Zsh..."
 sudo apt install -y zsh || { echo "Error installing zsh"; exit 1; }
 echo "Please provide input for configuration of Oh My Zsh"
+
+# subscript parce que sinon sa sort du script
+(
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" || { echo "Error installing Oh My Zsh"; exit 1; }
+)
+
+# lancement du config script
+# cré les symlinks approprié des dotfiles a home
+
+# symlink .zshrc
+if [ -f ~/.zshrc ]; then
+    rm -f ~/.zshrc || { echo "Erreur: impossible de supprimer .zshrc"; exit 1; }
+fi
+ln -s ~/.dotfiles/.zshrc ~/.zshrc || { echo "Erreur: problème lors de la création du symlink pour .zshrc"; exit 1; }
+
+# symlink .desktop shortcuts
+if [ -d ~/.local/share/applications ]; then
+    rm -rf ~/.local/share/applications || { echo "Erreur: impossible de supprimer le dossier applications"; exit 1; }
+fi
+ln -s ~/.dotfiles/applications ~/.local/share/applications || { echo "Erreur: problème lors de la création du symlink pour applications"; exit 1; }
+
+# symlink fonts
+if [ -d ~/.local/share/fonts ]; then
+    rm -rf ~/.local/share/fonts || { echo "Erreur: impossible de supprimer le dossier fonts"; exit 1; }
+fi
+ln -s ~/.dotfiles/fonts ~/.local/share/fonts || { echo "Erreur: problème lors de la création du symlink pour fonts"; exit 1; }
+
+# symlink .config
+if [ -d ~/.config ]; then
+    rm -rf ~/.config || { echo "Erreur: impossible de supprimer le dossier .config"; exit 1; }
+fi
+ln -s ~/.dotfiles/.config ~/.config || { echo "Erreur: problème lors de la création du symlink pour .config"; exit 1; }
+
+# Clone external plugins (Quickfix)
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || { echo "Erreur: problème lors du clonage de zsh-autosuggestions"; exit 1; }
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || { echo "Erreur: problème lors du clonage de zsh-syntax-highlighting"; exit 1; }
+git clone https://github.com/marlonrichert/zsh-autocomplete.git || { echo "Erreur: problème lors du clonage de zsh-autocomplete"; exit 1; }
+
+# fix manuel du dernier pluggin (Quickfix)
+mv ./zsh-autocomplete ~/.oh-my-zsh/plugins || { echo "erreur durant le fix zsh-autocomplete"; exit 1; }
+
+echo "Configurations réussi!"
